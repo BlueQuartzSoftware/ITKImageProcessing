8f1a58f0c511783a78a2b68b576382d6ec1a8180
Joey Kleingers <joey.kleingers@bluequartz.net>
2015-06-08 15:53:14 -0400

    Adding missing includes.

--- ZeissImportFilters/CalculateBackground.cpp
+++ ZeissImportFilters/CalculateBackground.cpp
@@ -20,6 +20,7 @@
 #include "DREAM3DLib/Common/IFilterFactory.hpp"
 #include "DREAM3DLib/FilterParameters/AbstractFilterParametersWriter.h"
 #include "DREAM3DLib/FilterParameters/AbstractFilterParametersReader.h"
+#include "DREAM3DLib/FilterParameters/SeparatorFilterParameter.h"
 
 
 
