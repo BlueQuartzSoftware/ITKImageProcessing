407cfe455ebb09a44f57ec7f6975e45f6cd1a3f4
Michael Jackson <mike.jackson@bluequartz.net>
2016-07-19 20:59:00 -0400

    CLEANUP: Removing header file that is not necessary since the new FilterParamter API was introduced.

--- ITKImageProcessingFilters/ITKImageProcessingFilter.cpp
+++ ITKImageProcessingFilters/ITKImageProcessingFilter.cpp
@@ -35,7 +35,6 @@
 
 #include "SIMPLib/Common/Constants.h"
 #include "SIMPLib/FilterParameters/AbstractFilterParametersReader.h"
-#include "SIMPLib/FilterParameters/AbstractFilterParametersWriter.h"
 
 
 
--- ITKImageProcessingFilters/ITKImageReader.cpp
+++ ITKImageProcessingFilters/ITKImageReader.cpp
@@ -35,7 +35,6 @@
 
 #include "SIMPLib/Common/Constants.h"
 #include "SIMPLib/FilterParameters/AbstractFilterParametersReader.h"
-#include "SIMPLib/FilterParameters/AbstractFilterParametersWriter.h"
 #include "SIMPLib/FilterParameters/InputFileFilterParameter.h"
 #include "SIMPLib/FilterParameters/StringFilterParameter.h"
 
