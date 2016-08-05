3f0d7c3d48e13289358cf54ad9eef3dd05cb8ee1
Joey Kleingers <joey.kleingers@bluequartz.net>
2016-08-05 11:14:09 -0400

    Replacing all DataArraySelectionFilterParameter::New function uses with its new macro.

--- ITKImageProcessingFilters/Itk_GaussianBlur.cpp
+++ ITKImageProcessingFilters/Itk_GaussianBlur.cpp
@@ -66,7 +66,7 @@ void Itk_GaussianBlur::setupFilterParameters()
     vec.push_back(SIMPL::TypeNames::Int32);
     vec.push_back(SIMPL::TypeNames::Int64);
     req.daTypes = vec;
-    parameters.push_back(DataArraySelectionFilterParameter::New("Attribute Array to Blur", "SelectedCellArrayPath", getSelectedCellArrayPath(), FilterParameter::RequiredArray, SIMPL_BIND_SETTER(Itk_GaussianBlur, this, SelectedCellArrayPath), SIMPL_BIND_GETTER(Itk_GaussianBlur, this, SelectedCellArrayPath), req));
+    parameters.push_back(SIMPL_NEW_DA_SELECTION_FP("Attribute Array to Blur", SelectedCellArrayPath, FilterParameter::RequiredArray, Itk_GaussianBlur, req));
   }
   parameters.push_back(SeparatorFilterParameter::New("Cell Data", FilterParameter::CreatedArray));
   parameters.push_back(StringFilterParameter::New("Blurred Array", "NewCellArrayName", getNewCellArrayName(), FilterParameter::CreatedArray, SIMPL_BIND_SETTER(Itk_GaussianBlur, this, NewCellArrayName), SIMPL_BIND_GETTER(Itk_GaussianBlur, this, NewCellArrayName)));
