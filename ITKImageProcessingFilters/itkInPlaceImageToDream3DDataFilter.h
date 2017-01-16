0b6f1e7bda4f8e1bae6b6c5ba3771d1b8aabebe4
Matt McCormick <matt.mccormick@kitware.com>
2017-01-16 13:50:02 -0500

    STYLE: Add additional whitespaces in Dream3ToITK filters
    For readability.

--- ITKImageProcessingFilters/itkInPlaceImageToDream3DDataFilter.h
+++ ITKImageProcessingFilters/itkInPlaceImageToDream3DDataFilter.h
@@ -18,9 +18,10 @@ namespace itk
   {
   public:
     /** Standard class typedefs. */
-    typedef InPlaceImageToDream3DDataFilter                                  Self;
-    typedef ProcessObject                                                    Superclass;
-    typedef SmartPointer<Self>                                               Pointer;
+    typedef InPlaceImageToDream3DDataFilter Self;
+    typedef ProcessObject                   Superclass;
+    typedef SmartPointer<Self>              Pointer;
+
     typedef typename itk::Dream3DImage<PixelType, VDimension>                ImageType;
     typedef typename ImageType::Pointer                                      ImagePointer;
     typedef typename itk::NumericTraits<PixelType>::ValueType                ValueType;
@@ -30,27 +31,36 @@ namespace itk
     /** Method for creation through the object factory. */
     itkNewMacro(Self);
     itkTypeMacro(InPlaceImageToDream3DDataFilter, ProcessObject);
+
     virtual void SetInput(const ImageType *image);
-    void SetDataContainer(DataContainer::Pointer dc);
+
     DecoratorType* GetOutput();
+
+    void SetDataContainer(DataContainer::Pointer dc);
+
     itkSetMacro(DataArrayName, std::string);
     itkGetMacro(DataArrayName, std::string);
+
     itkSetMacro(AttributeMatrixArrayName, std::string);
     itkGetMacro(AttributeMatrixArrayName, std::string);
+
     itkSetMacro( InPlace, bool );
     itkGetConstMacro(InPlace, bool);
+
     itkBooleanMacro(InPlace);
+
   protected:
-    virtual void VerifyPreconditions() ITK_OVERRIDE;
     InPlaceImageToDream3DDataFilter();
     virtual ~InPlaceImageToDream3DDataFilter();
 
+    virtual void VerifyPreconditions() ITK_OVERRIDE;
     ProcessObject::DataObjectPointer MakeOutput(ProcessObject::DataObjectPointerArraySizeType) override;
 
     virtual void GenerateData() ITK_OVERRIDE;
     virtual void GenerateOutputInformation() ITK_OVERRIDE;
 
     void CheckValidArrayPathComponentName(std::string var);
+
   private:
     using Superclass::SetInput;
     InPlaceImageToDream3DDataFilter(const InPlaceImageToDream3DDataFilter&); // Copy Constructor Not Implemented
