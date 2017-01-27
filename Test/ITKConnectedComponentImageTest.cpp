2e10381bf1c04d1303a21ac37b83f82e3ea4c283
Francois Budin <francois.budin@gmail.com>
2017-01-27 17:50:20 -0500

    ENH: Adding ITKConnectedComponentImage filter

---
+++ Test/ITKConnectedComponentImageTest.cpp
@@ -0,0 +1,113 @@
+// File automatically generated
+
+// -----------------------------------------------------------------------------
+// Insert your license & copyright information here
+// -----------------------------------------------------------------------------
+
+#include "ITKTestBase.h"
+//Auto includes
+#include <SIMPLib/FilterParameters/BooleanFilterParameter.h>
+
+
+class ITKConnectedComponentImageTest: public ITKTestBase
+{
+
+  public:
+    ITKConnectedComponentImageTest() {}
+    virtual ~ITKConnectedComponentImageTest() {}
+
+int TestITKConnectedComponentImagedefaultTest()
+{
+    QString input_filename = UnitTest::DataDir + QString("/Data/JSONFilters/Input/WhiteDots.png");
+    DataArrayPath input_path("TestContainer", "TestAttributeMatrixName", "TestAttributeArrayName");
+    DataContainerArray::Pointer containerArray = DataContainerArray::New();
+    this->ReadImage(input_filename, containerArray, input_path);
+    QString filtName = "ITKConnectedComponentImage";
+    FilterManager* fm = FilterManager::Instance();
+    IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+    DREAM3D_REQUIRE_NE(filterFactory.get(),0);
+    AbstractFilter::Pointer filter = filterFactory->create();
+    QVariant var;
+    bool propWasSet;
+    var.setValue(input_path);
+    propWasSet = filter->setProperty("SelectedCellArrayPath", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true);
+    var.setValue(false);
+    propWasSet = filter->setProperty("SaveAsNewArray", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true);
+    filter->setDataContainerArray(containerArray);
+    filter->execute();
+    DREAM3D_REQUIRED(filter->getErrorCondition(), >= , 0);
+    DREAM3D_REQUIRED(filter->getWarningCondition(), >= , 0);
+    WriteImage("ITKConnectedComponentImagedefault.nrrd", containerArray, input_path);
+    QString md5Output;
+    GetMD5FromDataContainer(containerArray, input_path, md5Output);
+    DREAM3D_REQUIRE_EQUAL(QString(md5Output), QString("548f5184428db10d93e3bf377dee5253"));
+    var = filter->property("ObjectCount");
+    DREAM3D_REQUIRE_EQUAL(var.toUInt(),23u);
+    return 0;
+}
+
+int TestITKConnectedComponentImagefullyconnectedTest()
+{
+    QString input_filename = UnitTest::DataDir + QString("/Data/JSONFilters/Input/WhiteDots.png");
+    DataArrayPath input_path("TestContainer", "TestAttributeMatrixName", "TestAttributeArrayName");
+    DataContainerArray::Pointer containerArray = DataContainerArray::New();
+    this->ReadImage(input_filename, containerArray, input_path);
+    QString filtName = "ITKConnectedComponentImage";
+    FilterManager* fm = FilterManager::Instance();
+    IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+    DREAM3D_REQUIRE_NE(filterFactory.get(),0);
+    AbstractFilter::Pointer filter = filterFactory->create();
+    QVariant var;
+    bool propWasSet;
+    var.setValue(input_path);
+    propWasSet = filter->setProperty("SelectedCellArrayPath", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true);
+    var.setValue(false);
+    propWasSet = filter->setProperty("SaveAsNewArray", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true);
+    {
+        bool d3d_var;
+        d3d_var = true;
+        var.setValue(d3d_var);
+        propWasSet = filter->setProperty("FullyConnected", var);
+        DREAM3D_REQUIRE_EQUAL(propWasSet, true);
+    }
+    filter->setDataContainerArray(containerArray);
+    filter->execute();
+    DREAM3D_REQUIRED(filter->getErrorCondition(), >= , 0);
+    DREAM3D_REQUIRED(filter->getWarningCondition(), >= , 0);
+    WriteImage("ITKConnectedComponentImagefullyconnected.nrrd", containerArray, input_path);
+    QString md5Output;
+    GetMD5FromDataContainer(containerArray, input_path, md5Output);
+    DREAM3D_REQUIRE_EQUAL(QString(md5Output), QString("e40b7cdfc1b34ae2e6b13660d626cc29"));
+    return 0;
+}
+
+
+
+
+  // -----------------------------------------------------------------------------
+  //
+  // -----------------------------------------------------------------------------
+  void operator()() override
+  {
+    int err = EXIT_SUCCESS;
+
+    DREAM3D_REGISTER_TEST( this->TestFilterAvailability("ITKConnectedComponentImage") );
+
+    DREAM3D_REGISTER_TEST( TestITKConnectedComponentImagedefaultTest());
+    DREAM3D_REGISTER_TEST( TestITKConnectedComponentImagefullyconnectedTest());
+
+    if(SIMPL::unittest::numTests == SIMPL::unittest::numTestsPass)
+    {
+      DREAM3D_REGISTER_TEST( this->RemoveTestFiles() )
+    }
+  }
+
+  private:
+    ITKConnectedComponentImageTest(const ITKConnectedComponentImageTest&); // Copy Constructor Not Implemented
+    void operator=(const ITKConnectedComponentImageTest&); // Operator '=' Not Implemented
+};
+
