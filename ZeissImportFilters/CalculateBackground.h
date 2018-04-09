e11f5c2d95b032f6eaef05c097fb97b05380e2a2
Michael Jackson <mike.jackson@bluequartz.net>
2018-04-09 10:19:46 -0400

    Adding Python bindings for all the external filters.

--- ZeissImportFilters/CalculateBackground.h
+++ ZeissImportFilters/CalculateBackground.h
@@ -25,161 +25,172 @@
 class CalculateBackground : public AbstractFilter
 {
   Q_OBJECT
+  PYB11_CREATE_BINDINGS(CalculateBackground SUPERCLASS AbstractFilter)
+  PYB11_PROPERTY(QString VolumeDataContainerName READ getVolumeDataContainerName WRITE setVolumeDataContainerName)
+  PYB11_PROPERTY(QString BackgroundAttributeMatrixName READ getBackgroundAttributeMatrixName WRITE setBackgroundAttributeMatrixName)
+  PYB11_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
+  PYB11_PROPERTY(DataArrayPath ImageDataArrayPath READ getImageDataArrayPath WRITE setImageDataArrayPath)
+  PYB11_PROPERTY(DataArrayPath AttributeMatrixName READ getAttributeMatrixName WRITE setAttributeMatrixName)
+  PYB11_PROPERTY(QString DataContainerBundleName READ getDataContainerBundleName WRITE setDataContainerBundleName)
+  PYB11_PROPERTY(QString BackgroundImageArrayName READ getBackgroundImageArrayName WRITE setBackgroundImageArrayName)
+  PYB11_PROPERTY(uint lowThresh READ getlowThresh WRITE setlowThresh)
+  PYB11_PROPERTY(uint highThresh READ gethighThresh WRITE sethighThresh)
+  PYB11_PROPERTY(bool SubtractBackground READ getSubtractBackground WRITE setSubtractBackground)
+  PYB11_PROPERTY(bool DivideBackground READ getDivideBackground WRITE setDivideBackground)
 
-  public:
-    SIMPL_SHARED_POINTERS(CalculateBackground)
-    SIMPL_FILTER_NEW_MACRO(CalculateBackground)
-    SIMPL_TYPE_MACRO_SUPER_OVERRIDE(CalculateBackground, AbstractFilter)
+public:
+  SIMPL_SHARED_POINTERS(CalculateBackground)
+  SIMPL_FILTER_NEW_MACRO(CalculateBackground)
+  SIMPL_TYPE_MACRO_SUPER_OVERRIDE(CalculateBackground, AbstractFilter)
 
-    ~CalculateBackground() override;
+  ~CalculateBackground() override;
 
-    SIMPL_INSTANCE_STRING_PROPERTY(DataContainerName)
+  SIMPL_INSTANCE_STRING_PROPERTY(DataContainerName)
 
+  SIMPL_FILTER_PARAMETER(QString, VolumeDataContainerName)
+  Q_PROPERTY(QString VolumeDataContainerName READ getVolumeDataContainerName WRITE setVolumeDataContainerName)
 
-    SIMPL_FILTER_PARAMETER(QString, VolumeDataContainerName)
-    Q_PROPERTY(QString VolumeDataContainerName READ getVolumeDataContainerName WRITE setVolumeDataContainerName)
+  SIMPL_FILTER_PARAMETER(QString, BackgroundAttributeMatrixName)
+  Q_PROPERTY(QString BackgroundAttributeMatrixName READ getBackgroundAttributeMatrixName WRITE setBackgroundAttributeMatrixName)
 
-    SIMPL_FILTER_PARAMETER(QString, BackgroundAttributeMatrixName)
-    Q_PROPERTY(QString BackgroundAttributeMatrixName READ getBackgroundAttributeMatrixName WRITE setBackgroundAttributeMatrixName)
+  SIMPL_FILTER_PARAMETER(QString, CellAttributeMatrixName)
+  Q_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
 
-    SIMPL_FILTER_PARAMETER(QString, CellAttributeMatrixName)
-    Q_PROPERTY(QString CellAttributeMatrixName READ getCellAttributeMatrixName WRITE setCellAttributeMatrixName)
+  SIMPL_FILTER_PARAMETER(DataArrayPath, ImageDataArrayPath)
+  Q_PROPERTY(DataArrayPath ImageDataArrayPath READ getImageDataArrayPath WRITE setImageDataArrayPath)
 
-    SIMPL_FILTER_PARAMETER(DataArrayPath, ImageDataArrayPath)
-    Q_PROPERTY(DataArrayPath ImageDataArrayPath READ getImageDataArrayPath WRITE setImageDataArrayPath)
+  SIMPL_FILTER_PARAMETER(DataArrayPath, AttributeMatrixName)
+  Q_PROPERTY(DataArrayPath AttributeMatrixName READ getAttributeMatrixName WRITE setAttributeMatrixName)
 
-    SIMPL_FILTER_PARAMETER(DataArrayPath, AttributeMatrixName)
-    Q_PROPERTY(DataArrayPath AttributeMatrixName READ getAttributeMatrixName WRITE setAttributeMatrixName)
+  SIMPL_FILTER_PARAMETER(QString, DataContainerBundleName)
+  Q_PROPERTY(QString DataContainerBundleName READ getDataContainerBundleName WRITE setDataContainerBundleName)
 
-    SIMPL_FILTER_PARAMETER(QString, DataContainerBundleName)
-    Q_PROPERTY(QString DataContainerBundleName READ getDataContainerBundleName WRITE setDataContainerBundleName)
+  SIMPL_FILTER_PARAMETER(QString, BackgroundImageArrayName)
+  Q_PROPERTY(QString BackgroundImageArrayName READ getBackgroundImageArrayName WRITE setBackgroundImageArrayName)
 
-    SIMPL_FILTER_PARAMETER(QString, BackgroundImageArrayName)
-    Q_PROPERTY(QString BackgroundImageArrayName READ getBackgroundImageArrayName WRITE setBackgroundImageArrayName)
+  SIMPL_FILTER_PARAMETER(uint, lowThresh)
+  Q_PROPERTY(uint lowThresh READ getlowThresh WRITE setlowThresh)
 
-    SIMPL_FILTER_PARAMETER(uint, lowThresh)
-    Q_PROPERTY(uint lowThresh READ getlowThresh WRITE setlowThresh)
+  SIMPL_FILTER_PARAMETER(uint, highThresh)
+  Q_PROPERTY(uint highThresh READ gethighThresh WRITE sethighThresh)
 
-    SIMPL_FILTER_PARAMETER(uint, highThresh)
-    Q_PROPERTY(uint highThresh READ gethighThresh WRITE sethighThresh)
+  SIMPL_FILTER_PARAMETER(bool, SubtractBackground)
+  Q_PROPERTY(int SubtractBackground READ getSubtractBackground WRITE setSubtractBackground)
 
-    SIMPL_FILTER_PARAMETER(bool, SubtractBackground)
-    Q_PROPERTY(int SubtractBackground READ getSubtractBackground WRITE setSubtractBackground)
+  SIMPL_FILTER_PARAMETER(bool, DivideBackground)
+  Q_PROPERTY(int DivideBackground READ getDivideBackground WRITE setDivideBackground)
 
-    SIMPL_FILTER_PARAMETER(bool, DivideBackground)
-    Q_PROPERTY(int DivideBackground READ getDivideBackground WRITE setDivideBackground)
+  /**
+   * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
+   */
+  const QString getCompiledLibraryName() const override;
 
-    /**
-     * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
-     */
-    const QString getCompiledLibraryName() const override;
+  /**
+   * @brief getBrandingString Returns the branding string for the filter, which is a tag
+   * used to denote the filter's association with specific plugins
+   * @return Branding string
+   */
+  const QString getBrandingString() const override;
 
-    /**
-     * @brief getBrandingString Returns the branding string for the filter, which is a tag
-     * used to denote the filter's association with specific plugins
-     * @return Branding string
-    */
-    const QString getBrandingString() const override;
+  /**
+   * @brief getFilterVersion Returns a version string for this filter. Default
+   * value is an empty string.
+   * @return
+   */
+  const QString getFilterVersion() const override;
 
-    /**
-     * @brief getFilterVersion Returns a version string for this filter. Default
-     * value is an empty string.
-     * @return
-     */
-    const QString getFilterVersion() const override;
+  /**
+   * @brief newFilterInstance Reimplemented from @see AbstractFilter class
+   */
+  AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
 
-    /**
-     * @brief newFilterInstance Reimplemented from @see AbstractFilter class
-     */
-    AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
+  /**
+   * @brief getGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getGroupName() const override;
 
-    /**
-     * @brief getGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getGroupName() const override;
+  /**
+   * @brief getSubGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getSubGroupName() const override;
 
-    /**
-     * @brief getSubGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getSubGroupName() const override;
+  /**
+   * @brief getUuid Return the unique identifier for this filter.
+   * @return A QUuid object.
+   */
+  const QUuid getUuid() override;
 
-    /**
-     * @brief getUuid Return the unique identifier for this filter.
-     * @return A QUuid object.
-     */
-    const QUuid getUuid() override;
+  /**
+   * @brief getHumanLabel Reimplemented from @see AbstractFilter class
+   */
+  const QString getHumanLabel() const override;
 
-    /**
-     * @brief getHumanLabel Reimplemented from @see AbstractFilter class
-     */
-    const QString getHumanLabel() const override;
+  /**
+   * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
+   */
+  void setupFilterParameters() override;
 
-    /**
-     * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
-     */
-    void setupFilterParameters() override;
+  /**
+   * @brief readFilterParameters Reimplemented from @see AbstractFilter class
+   */
+  void readFilterParameters(AbstractFilterParametersReader* reader, int index);
 
-    /**
-     * @brief readFilterParameters Reimplemented from @see AbstractFilter class
-     */
-    void readFilterParameters(AbstractFilterParametersReader* reader, int index);
+  /**
+   * @brief execute Reimplemented from @see AbstractFilter class
+   */
+  void execute() override;
 
-    /**
-     * @brief execute Reimplemented from @see AbstractFilter class
-     */
-    void execute() override;
+  /**
+   * @brief preflight Reimplemented from @see AbstractFilter class
+   */
+  void preflight() override;
 
-    /**
-    * @brief preflight Reimplemented from @see AbstractFilter class
-    */
-    void preflight() override;
+signals:
+  /**
+   * @brief updateFilterParameters This is emitted when the filter requests all the latest Filter Parameters need to be
+   * pushed from a user facing control such as the FilterParameter Widget
+   * @param filter The filter to push the values into
+   */
+  void updateFilterParameters(AbstractFilter* filter);
 
-  signals:
-    /**
-     * @brief updateFilterParameters This is emitted when the filter requests all the latest Filter Parameters need to be
-     * pushed from a user facing control such as the FilterParameter Widget
-     * @param filter The filter to push the values into
-     */
-    void updateFilterParameters(AbstractFilter* filter);
+  /**
+   * @brief parametersChanged This signal can be emitted when any of the filter parameters are changed internally.
+   */
+  void parametersChanged();
 
-    /**
-     * @brief parametersChanged This signal can be emitted when any of the filter parameters are changed internally.
-     */
-    void parametersChanged();
+  /**
+   * @brief preflightAboutToExecute Emitted just before the dataCheck() is called. This can change if needed.
+   */
+  void preflightAboutToExecute();
 
-    /**
-     * @brief preflightAboutToExecute Emitted just before the dataCheck() is called. This can change if needed.
-     */
-    void preflightAboutToExecute();
+  /**
+   * @brief preflightExecuted Emitted just after the dataCheck() is called. Typically. This can change if needed.
+   */
+  void preflightExecuted();
 
-    /**
-     * @brief preflightExecuted Emitted just after the dataCheck() is called. Typically. This can change if needed.
-     */
-    void preflightExecuted();
+protected:
+  CalculateBackground();
 
-  protected:
-    CalculateBackground();
+  /**
+   * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
+   */
+  void dataCheck();
 
-    /**
-     * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
-     */
-    void dataCheck();
+  /**
+   * @brief Initializes all the private instance variables.
+   */
+  void initialize();
 
-    /**
-     * @brief Initializes all the private instance variables.
-     */
-    void initialize();
+private:
+  int64_t m_TotalPoints;
 
-  private:
-    int64_t m_TotalPoints;
+  DEFINE_DATAARRAY_VARIABLE(double, BackgroundImage)
 
-    DEFINE_DATAARRAY_VARIABLE(double, BackgroundImage)
-
-  public:
-    CalculateBackground(const CalculateBackground&) = delete; // Copy Constructor Not Implemented
-    CalculateBackground(CalculateBackground&&) = delete;      // Move Constructor
-    CalculateBackground& operator=(const CalculateBackground&) = delete; // Copy Assignment Not Implemented
-    CalculateBackground& operator=(CalculateBackground&&) = delete;      // Move Assignment
+public:
+  CalculateBackground(const CalculateBackground&) = delete;            // Copy Constructor Not Implemented
+  CalculateBackground(CalculateBackground&&) = delete;                 // Move Constructor
+  CalculateBackground& operator=(const CalculateBackground&) = delete; // Copy Assignment Not Implemented
+  CalculateBackground& operator=(CalculateBackground&&) = delete;      // Move Assignment
 };
 
 #endif /* _CalculateBackground_H_ */
--- ZeissImportFilters/ZeissImportFilter.h
+++ ZeissImportFilters/ZeissImportFilter.h
@@ -31,225 +31,230 @@ class ZeissImportFilterPrivate;
 class ZeissImportFilter : public AbstractFilter
 {
   Q_OBJECT
-    Q_DECLARE_PRIVATE(ZeissImportFilter)
+  PYB11_CREATE_BINDINGS(ZeissImportFilter SUPERCLASS AbstractFilter)
+  PYB11_PROPERTY(QString InputFile READ getInputFile WRITE setInputFile)
+  PYB11_PROPERTY(QString DataContainerName READ getDataContainerName WRITE setDataContainerName)
+  PYB11_PROPERTY(QString ImageAttributeMatrixName READ getImageAttributeMatrixName WRITE setImageAttributeMatrixName)
+  PYB11_PROPERTY(QString ImageDataArrayPrefix READ getImageDataArrayPrefix WRITE setImageDataArrayPrefix)
+  PYB11_PROPERTY(bool ConvertToGrayScale READ getConvertToGrayScale WRITE setConvertToGrayScale)
+  PYB11_PROPERTY(FloatVec3_t ColorWeights READ getColorWeights WRITE setColorWeights)
+  PYB11_PROPERTY(bool FileWasRead READ getFileWasRead WRITE setFileWasRead)
+  Q_DECLARE_PRIVATE(ZeissImportFilter)
 
-  public:
-    SIMPL_SHARED_POINTERS(ZeissImportFilter)
-    SIMPL_FILTER_NEW_MACRO(ZeissImportFilter)
-    SIMPL_TYPE_MACRO_SUPER_OVERRIDE(ZeissImportFilter, AbstractFilter)
+public:
+  SIMPL_SHARED_POINTERS(ZeissImportFilter)
+  SIMPL_FILTER_NEW_MACRO(ZeissImportFilter)
+  SIMPL_TYPE_MACRO_SUPER_OVERRIDE(ZeissImportFilter, AbstractFilter)
 
-    ~ZeissImportFilter() override;
+  ~ZeissImportFilter() override;
 
-    SIMPL_FILTER_PARAMETER(QString, InputFile)
-    Q_PROPERTY(QString InputFile READ getInputFile WRITE setInputFile)
+  SIMPL_FILTER_PARAMETER(QString, InputFile)
+  Q_PROPERTY(QString InputFile READ getInputFile WRITE setInputFile)
 
-    SIMPL_FILTER_PARAMETER(QString, DataContainerName)
-    Q_PROPERTY(QString DataContainerName READ getDataContainerName WRITE setDataContainerName)
+  SIMPL_FILTER_PARAMETER(QString, DataContainerName)
+  Q_PROPERTY(QString DataContainerName READ getDataContainerName WRITE setDataContainerName)
 
-    SIMPL_FILTER_PARAMETER(QString, ImageAttributeMatrixName)
-    Q_PROPERTY(QString ImageAttributeMatrixName READ getImageAttributeMatrixName WRITE setImageAttributeMatrixName)
+  SIMPL_FILTER_PARAMETER(QString, ImageAttributeMatrixName)
+  Q_PROPERTY(QString ImageAttributeMatrixName READ getImageAttributeMatrixName WRITE setImageAttributeMatrixName)
 
-    SIMPL_FILTER_PARAMETER(QString, ImageDataArrayPrefix)
-    Q_PROPERTY(QString ImageDataArrayPrefix READ getImageDataArrayPrefix WRITE setImageDataArrayPrefix)
+  SIMPL_FILTER_PARAMETER(QString, ImageDataArrayPrefix)
+  Q_PROPERTY(QString ImageDataArrayPrefix READ getImageDataArrayPrefix WRITE setImageDataArrayPrefix)
 
-    SIMPL_FILTER_PARAMETER(bool, ConvertToGrayScale)
-    Q_PROPERTY(bool ConvertToGrayScale READ getConvertToGrayScale WRITE setConvertToGrayScale)
+  SIMPL_FILTER_PARAMETER(bool, ConvertToGrayScale)
+  Q_PROPERTY(bool ConvertToGrayScale READ getConvertToGrayScale WRITE setConvertToGrayScale)
 
-    SIMPL_FILTER_PARAMETER(FloatVec3_t, ColorWeights)
-    Q_PROPERTY(FloatVec3_t ColorWeights READ getColorWeights WRITE setColorWeights)
+  SIMPL_FILTER_PARAMETER(FloatVec3_t, ColorWeights)
+  Q_PROPERTY(FloatVec3_t ColorWeights READ getColorWeights WRITE setColorWeights)
 
-    SIMPL_FILTER_PARAMETER(bool, FileWasRead)
-    Q_PROPERTY(bool FileWasRead READ getFileWasRead)
+  SIMPL_FILTER_PARAMETER(bool, FileWasRead)
+  Q_PROPERTY(bool FileWasRead READ getFileWasRead)
 
-    /**
-     * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
-     */
-    const QString getCompiledLibraryName() const override;
+  /**
+   * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
+   */
+  const QString getCompiledLibraryName() const override;
 
-    /**
-     * @brief getBrandingString Returns the branding string for the filter, which is a tag
-     * used to denote the filter's association with specific plugins
-     * @return Branding string
-    */
-    const QString getBrandingString() const override;
+  /**
+   * @brief getBrandingString Returns the branding string for the filter, which is a tag
+   * used to denote the filter's association with specific plugins
+   * @return Branding string
+   */
+  const QString getBrandingString() const override;
 
-    /**
-     * @brief getFilterVersion Returns a version string for this filter. Default
-     * value is an empty string.
-     * @return
-     */
-    const QString getFilterVersion() const override;
+  /**
+   * @brief getFilterVersion Returns a version string for this filter. Default
+   * value is an empty string.
+   * @return
+   */
+  const QString getFilterVersion() const override;
 
-    /**
-     * @brief newFilterInstance Reimplemented from @see AbstractFilter class
-     */
-    AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
+  /**
+   * @brief newFilterInstance Reimplemented from @see AbstractFilter class
+   */
+  AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) const override;
 
-    /**
-     * @brief getGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getGroupName() const override;
+  /**
+   * @brief getGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getGroupName() const override;
 
-    /**
-     * @brief getSubGroupName Reimplemented from @see AbstractFilter class
-     */
-    const QString getSubGroupName() const override;
+  /**
+   * @brief getSubGroupName Reimplemented from @see AbstractFilter class
+   */
+  const QString getSubGroupName() const override;
 
-    /**
-     * @brief getUuid Return the unique identifier for this filter.
-     * @return A QUuid object.
-     */
-    const QUuid getUuid() override;
+  /**
+   * @brief getUuid Return the unique identifier for this filter.
+   * @return A QUuid object.
+   */
+  const QUuid getUuid() override;
 
-    /**
-     * @brief getHumanLabel Reimplemented from @see AbstractFilter class
-     */
-    const QString getHumanLabel() const override;
+  /**
+   * @brief getHumanLabel Reimplemented from @see AbstractFilter class
+   */
+  const QString getHumanLabel() const override;
 
-    /**
-     * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
-     */
-    void setupFilterParameters() override;
+  /**
+   * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
+   */
+  void setupFilterParameters() override;
 
-    /**
-     * @brief readFilterParameters Reimplemented from @see AbstractFilter class
-     */
-    void readFilterParameters(AbstractFilterParametersReader* reader, int index);
+  /**
+   * @brief readFilterParameters Reimplemented from @see AbstractFilter class
+   */
+  void readFilterParameters(AbstractFilterParametersReader* reader, int index);
 
-    /**
-    * @brief readFilterParametersFromJson Reads the filter parameters from a file
-    * @param reader Reader that is used to read the parameters from a file
-    */
-    virtual void readFilterParameters(QJsonObject &obj);
+  /**
+   * @brief readFilterParametersFromJson Reads the filter parameters from a file
+   * @param reader Reader that is used to read the parameters from a file
+   */
+  virtual void readFilterParameters(QJsonObject& obj);
 
-    /**
-    * @brief writeFilterParametersToJson Writes the filter parameters to a file
-    * @param root The root json object
-    */
-    virtual void writeFilterParameters(QJsonObject &obj);
+  /**
+   * @brief writeFilterParametersToJson Writes the filter parameters to a file
+   * @param root The root json object
+   */
+  virtual void writeFilterParameters(QJsonObject& obj);
 
-    /**
-     * @brief execute Reimplemented from @see AbstractFilter class
-     */
-    void execute() override;
+  /**
+   * @brief execute Reimplemented from @see AbstractFilter class
+   */
+  void execute() override;
 
-    /**
-    * @brief preflight Reimplemented from @see AbstractFilter class
-    */
-    void preflight() override;
+  /**
+   * @brief preflight Reimplemented from @see AbstractFilter class
+   */
+  void preflight() override;
 
-    SIMPL_PIMPL_PROPERTY_DECL(QDomElement, Root)
-    SIMPL_PIMPL_PROPERTY_DECL(ZeissTagsXmlSection::Pointer, RootTagsSection)
-    SIMPL_PIMPL_PROPERTY_DECL(QString, InputFile_Cache)
-    SIMPL_PIMPL_PROPERTY_DECL(QDateTime, LastRead)
+  SIMPL_PIMPL_PROPERTY_DECL(QDomElement, Root)
+  SIMPL_PIMPL_PROPERTY_DECL(ZeissTagsXmlSection::Pointer, RootTagsSection)
+  SIMPL_PIMPL_PROPERTY_DECL(QString, InputFile_Cache)
+  SIMPL_PIMPL_PROPERTY_DECL(QDateTime, LastRead)
 
-  signals:
-    /**
-     * @brief updateFilterParameters This is emitted when the filter requests all the latest Filter Parameters need to be
-     * pushed from a user facing control such as the FilterParameter Widget
-     * @param filter The filter to push the values into
-     */
-    void updateFilterParameters(AbstractFilter* filter);
+signals:
+  /**
+   * @brief updateFilterParameters This is emitted when the filter requests all the latest Filter Parameters need to be
+   * pushed from a user facing control such as the FilterParameter Widget
+   * @param filter The filter to push the values into
+   */
+  void updateFilterParameters(AbstractFilter* filter);
 
-    /**
-     * @brief parametersChanged This signal can be emitted when any of the filter parameters are changed internally.
-     */
-    void parametersChanged();
+  /**
+   * @brief parametersChanged This signal can be emitted when any of the filter parameters are changed internally.
+   */
+  void parametersChanged();
 
-    /**
-     * @brief preflightAboutToExecute Emitted just before the dataCheck() is called. This can change if needed.
-     */
-    void preflightAboutToExecute();
+  /**
+   * @brief preflightAboutToExecute Emitted just before the dataCheck() is called. This can change if needed.
+   */
+  void preflightAboutToExecute();
 
-    /**
-     * @brief preflightExecuted Emitted just after the dataCheck() is called. Typically. This can change if needed.
-     */
-    void preflightExecuted();
+  /**
+   * @brief preflightExecuted Emitted just after the dataCheck() is called. Typically. This can change if needed.
+   */
+  void preflightExecuted();
 
-  protected:
-    ZeissImportFilter();
+protected:
+  ZeissImportFilter();
 
-    /**
-     * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
-     */
-    void dataCheck();
+  /**
+   * @brief dataCheck Checks for the appropriate parameter values and availability of arrays
+   */
+  void dataCheck();
 
-    /**
-     * @brief Initializes all the private instance variables.
-     */
-    void initialize();
+  /**
+   * @brief Initializes all the private instance variables.
+   */
+  void initialize();
 
+  /**
+   * @brief readMetaXml
+   * @param device
+   * @return
+   */
+  int readMetaXml(QIODevice* device);
 
-    /**
-     * @brief readMetaXml
-     * @param device
-     * @return
-     */
-    int readMetaXml(QIODevice* device);
+  /**
+   * @brief parseRootTagsSection
+   * @param tags
+   * @return
+   */
+  ZeissTagsXmlSection::Pointer parseTagsSection(QDomElement& tags);
 
-    /**
-     * @brief parseRootTagsSection
-     * @param tags
-     * @return
-     */
-    ZeissTagsXmlSection::Pointer parseTagsSection(QDomElement& tags);
+  /**
+   * @brief parseImages
+   * @param rootTags
+   */
+  void parseImages(QDomElement& root, ZeissTagsXmlSection::Pointer rootTagsSection);
 
-    /**
-     * @brief parseImages
-     * @param rootTags
-     */
-    void parseImages(QDomElement& root, ZeissTagsXmlSection::Pointer rootTagsSection);
+  /**
+   * @brief getImageDimensions
+   * @param photoTagsSection
+   * @return
+   */
+  QVector<size_t> getImageDimensions(ZeissTagsXmlSection::Pointer photoTagsSection);
 
-    /**
-     * @brief getImageDimensions
-     * @param photoTagsSection
-     * @return
-     */
-    QVector<size_t> getImageDimensions(ZeissTagsXmlSection::Pointer photoTagsSection);
+  /**
+   * @brief generateMetaDataAttributeMatrix
+   * @param photoTagsSection
+   * @param imageCount
+   * @return
+   */
+  void addMetaData(AttributeMatrix::Pointer metaAm, ZeissTagsXmlSection::Pointer photoTagsSection, int index);
 
-    /**
-     * @brief generateMetaDataAttributeMatrix
-     * @param photoTagsSection
-     * @param imageCount
-     * @return
-     */
-    void addMetaData(AttributeMatrix::Pointer metaAm, ZeissTagsXmlSection::Pointer photoTagsSection, int index);
+  /**
+   * @brief generateMetaDataAttributeMatrix -root data
+   * @param rootTagsSection
+   * @param imageCount
+   * @return
+   */
 
-    /**
-     * @brief generateMetaDataAttributeMatrix -root data
-     * @param rootTagsSection
-     * @param imageCount
-     * @return
-     */
+  void addRootMetaData(AttributeMatrix::Pointer metaAm, ZeissTagsXmlSection::Pointer rootTagsSection, int index);
 
-    void addRootMetaData(AttributeMatrix::Pointer metaAm, ZeissTagsXmlSection::Pointer rootTagsSection, int index);
+  /**
+   * @brief generateDataArrays
+   * @param imageName
+   * @param pTag
+   * @param dcName
+   */
+  void importImage(const QString& imageName, const QString& pTag, const QString& dcName, int imageIndex, StringDataArray::Pointer attributeArrayNames);
 
-    /**
-     * @brief generateDataArrays
-     * @param imageName
-     * @param pTag
-     * @param dcName
-     */
-    void importImage(const QString &imageName, const QString &pTag,
-                     const QString &dcName, int imageIndex, StringDataArray::Pointer attributeArrayNames);
+  /**
+   * @brief convertToGrayScale
+   * @param imageName
+   * @param pTag
+   * @param dcName
+   */
+  void convertToGrayScale(const QString& imageName, const QString& pTag, const QString& dcName);
 
-    /**
-     * @brief convertToGrayScale
-     * @param imageName
-     * @param pTag
-     * @param dcName
-     */
-    void convertToGrayScale(const QString &imageName, const QString &pTag, const QString &dcName);
+private:
+  QScopedPointer<ZeissImportFilterPrivate> const d_ptr;
 
-
-  private:
-    QScopedPointer<ZeissImportFilterPrivate> const d_ptr;
-
-  public:
-    ZeissImportFilter(const ZeissImportFilter&) = delete; // Copy Constructor Not Implemented
-    ZeissImportFilter(ZeissImportFilter&&) = delete;      // Move Constructor
-    ZeissImportFilter& operator=(const ZeissImportFilter&) = delete; // Copy Assignment Not Implemented
-    ZeissImportFilter& operator=(ZeissImportFilter&&) = delete;      // Move Assignment
+public:
+  ZeissImportFilter(const ZeissImportFilter&) = delete;            // Copy Constructor Not Implemented
+  ZeissImportFilter(ZeissImportFilter&&) = delete;                 // Move Constructor
+  ZeissImportFilter& operator=(const ZeissImportFilter&) = delete; // Copy Assignment Not Implemented
+  ZeissImportFilter& operator=(ZeissImportFilter&&) = delete;      // Move Assignment
 };
 
 #endif /* _ZeissImportFilter_H_ */
