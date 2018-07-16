59786007fc729f23d51ae626afb3809be2f38851
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-16 14:01:38 -0400

    Fix issue where filters would throw error when canceled leading to crashes downstream
    fixes #157. updates #157. closes #157

--- ITKImageProcessingFilters/ITKImageBase.h
+++ ITKImageProcessingFilters/ITKImageBase.h
@@ -204,10 +204,13 @@ protected:
       toDream3DFilter->SetDataContainer(dc);
       toDream3DFilter->Update();
     } catch(itk::ExceptionObject& err)
-    {
-      setErrorCondition(-55555);
-      QString errorMessage = "ITK exception was thrown while filtering input image: %1";
-      notifyErrorMessage(getHumanLabel(), errorMessage.arg(err.GetDescription()), getErrorCondition());
+    { 
+      if(!getCancel())
+      {
+        setErrorCondition(-55555);
+        QString errorMessage = "ITK exception was thrown while filtering input image: %1";
+        notifyErrorMessage(getHumanLabel(), errorMessage.arg(err.GetDescription()), getErrorCondition());
+      }
       return;
     }
 
