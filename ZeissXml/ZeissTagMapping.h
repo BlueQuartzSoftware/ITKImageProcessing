7e6bd046e134691024d038d3bed1ce4359c329a0
Michael Jackson <mike.jackson@bluequartz.net>
2019-11-27 11:25:57 -0500

    Fix alias to use a const where appropriate
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- ZeissXml/ZeissTagMapping.h
+++ ZeissXml/ZeissTagMapping.h
@@ -63,7 +63,7 @@ class ITKImageProcessing_EXPORT ZeissTagMapping
     using Pointer = std::shared_ptr<Self>;
     using ConstPointer = std::shared_ptr<const Self>;
     using WeakPointer = std::weak_ptr<Self>;
-    using ConstWeakPointer = std::weak_ptr<Self>;
+    using ConstWeakPointer = std::weak_ptr<const Self>;
     static Pointer NullPointer();
 
     ~ZeissTagMapping();
@@ -112,7 +112,7 @@ public:
   using Pointer = std::shared_ptr<Self>;
   using ConstPointer = std::shared_ptr<const Self>;
   using WeakPointer = std::weak_ptr<Self>;
-  using ConstWeakPointer = std::weak_ptr<Self>;
+  using ConstWeakPointer = std::weak_ptr<const Self>;
   static Pointer NullPointer();
 
   ~ZeissUnitMapping();
