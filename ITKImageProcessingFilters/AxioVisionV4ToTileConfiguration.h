76056bfe87482ff7418cfbfe3782cfef00e84777
Michael Jackson <mike.jackson@bluequartz.net>
2020-07-17 14:33:31 -0400

    Correct comment to remove dead URL.
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- ITKImageProcessingFilters/AxioVisionV4ToTileConfiguration.h
+++ ITKImageProcessingFilters/AxioVisionV4ToTileConfiguration.h
@@ -192,7 +192,7 @@ private:
 
 public:
   /* Rule of 5: All special member functions should be defined if any are defined.
-   * https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#c21-if-you-define-or-delete-any-default-operation-define-or-delete-them-all
+   * CppCoreGuidelines #c21 if you define or delete any default operation define or delete them all
    */
   AxioVisionV4ToTileConfiguration(const AxioVisionV4ToTileConfiguration&) = delete;            // Copy Constructor Not Implemented
   AxioVisionV4ToTileConfiguration& operator=(const AxioVisionV4ToTileConfiguration&) = delete; // Copy Assignment Not Implemented
