21db03f75362e812006cf6750af2dc7443fd7304
Jared Duffey <jared.duffey@bluequartz.net>
2021-08-09 13:33:16 -0400

    Added python binding for VectorFileListInfo_t
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

---
+++ Python/pyBodyTop.cpp
@@ -0,0 +1,3 @@
+py::class_<VectorFileListInfo_t> instanceVectorFileListInfo(mod, "VectorFileListInfo");
+
+instanceVectorFileListInfo.def(py::init<int32_t, uint32_t, int32_t, int32_t, int32_t, int32_t, int32_t, QString, QString, QString, QString, QString>());
